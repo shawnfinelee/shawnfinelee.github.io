fef802ecb3f76c06559e8e9adb26a46bc712c6ac
lee <shawnfinelee@163.com>
2024-06-14 12:35:11 +0800

    update blog

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,12 @@ echo -e "Deploying..."
 hugo --environment production
 git add .
 git status
-read -p "Enter git commit message: " msg
-if [ -z $msg ];then
-  msg="update $(date +'%F %a %T')"
-fi
-git commit -m "$msg"
+# read -p "Enter git commit message: " msg
+# if [ -z $msg ];then
+#   msg="update $(date +'%F %a %T')"
+# fi
+# git commit -m "$msg"
+git commit -m "update blog"
 echo -e "Pushing..."
 git push
 echo -e "View at https://shawnfinelee.github.io"
